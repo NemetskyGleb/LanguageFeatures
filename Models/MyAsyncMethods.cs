494ea3632a87d90bcf541911ef220e2bc88eaf30
Gleb <glebdvp@gmail.com>
2022-01-10 10:49:03 +0700

    Применение async и await

--- Models/MyAsyncMethods.cs
+++ Models/MyAsyncMethods.cs
@@ -5,28 +5,21 @@ namespace LanguageFeatures.Models;
 
 public class MyAsyncMethods 
 {
-    public static Task<long?> GetPageLength()
+    public async static Task<long?> GetPageLength()
     {
         HttpClient client = new HttpClient();
-        var httpTask = client.GetAsync("http://apress.com");
-        return httpTask.ContinueWith((Task<HttpResponseMessage> antecedent) =>
-        {
-            return antecedent.Result.Content.Headers.ContentLength;
-        });
+        var httpMessage = await client.GetAsync("http://apress.com");
+        return httpMessage.Content.Headers.ContentLength;
     }
 }
-// Метод использует объект System.Net.Http.HttpClient для запрашивания
-// содержимого домашней страницы издательства Apress
-// и возвращает длину полученного содержимого.
-// Работа, которая будет выполняться асинхронно представлена в .NET
-// как объект Task. Объекты Task строго типизируются на основе результата,
-// выдаваемого фоновой работой. Таким образом, при вызове метода HttpClient.GetAsync()
-// получается объект Task<HttpResponseMessage>. Он сообщает о том, что запрос выполнится 
-// в фоновом режиме и его результатом будет объект HttpResponseMessage.
-// Далее идет механизм, с помощью которого указывается то, что должно
-// произойти, когда фоновая задача завершится.
-// Первое применение ключевого слова return указывает,
-// что возвращается объект Task<HttpResponseMessage>. который при завершении
-// задачи возвратит (второе ключевое слово return) длину заголовка ContentLength.
-// Заголовок ContentLength возвращает значение long? (тип long, допускающий null),
-//  т.е. результатом метода GetPageLength () является Task<long?>
+// Ключевое слово await используется при вызове асинхронного метода. 
+// Оно сообщает компилятору С# о том, что необходимо подождать результата Task, 
+// который возвращается методом GetAsync(),
+// и затем заняться выполнением остальных операторов в том же методе.
+// Применение ключевого слова await означает, что мы можем трактовать результат
+// метода GetAsync().как если бы он был обычным методом, 
+// и просто присвоить возвращаемый им объект HttpResponseMessage какой-нибудь переменной.
+// Еще лучше то, что затем можно использовать ключевое слово return традиционным образом для
+// выдачи результата из другого метода - значения свойства ContentLength в данном
+// случае.
+// При использовании ключевого слова await потребуется также добавить к сигнатуре метода ключевое слово async
